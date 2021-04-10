708c1cf9dc7acbbad9a2c457a171663eedc24152
ArmyAntMF54 <anthonyjpadgett@gmail.com>
2021-04-09 22:08:42 -0500

    Task Done

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StaticClassAttributes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Song woah = new Song("Woah","Koko", 175);
+            Console.WriteLine(Song.songCount);
+            Song hey = new Song("Hey", "Tony", 220);
+
+            Console.WriteLine(hey.getSongCount());// static class attribute is about the CLASS, not the objects of the class.
+        }
+    }
+}
